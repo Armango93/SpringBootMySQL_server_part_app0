5b337cd281b60193107d14fae8b63b0f424a4558
Andrew Nikolaev <nikolaeff.expert@gmail.com>
2018-07-11 17:02:37 +0700

    Initial commit

---
+++ src/main/java/com/spring/boot/mysql/controller/model/Book.java
@@ -0,0 +1,73 @@
+package com.spring.boot.mysql.controller.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "books")
+public class Book implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@Column(name = "title")
+	private String title;
+
+	@Column(name = "author")
+	private String author;
+
+	@Column(name = "description")
+	private String description;
+
+	@Column(name = "published")
+	private int published;
+
+	protected Book() {
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getPublished() {
+		return published;
+	}
+
+	public void setPublished(int published) {
+		this.published = published;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", description=" + description
+				+ ", published=" + published + "]";
+	}
+
+}
